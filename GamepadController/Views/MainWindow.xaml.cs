08ad89a0d589b769aac7ce73dba3d43fd31c339c
Sadness96 <qsbs623@163.com>
2021-05-22 01:51:03 +0800

    修改：主界面跳转样式

--- GamepadController/Views/MainWindow.xaml.cs
+++ GamepadController/Views/MainWindow.xaml.cs
@@ -22,6 +22,35 @@ namespace GamepadController.Views
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            QuanbaQ1.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/QuanbaQ1.jpg")));
+            XboxOne.Background = new ImageBrush(new BitmapImage(new Uri(@"pack://application:,,,/Images/XboxOne.jpg")));
+        }
+
+        /// <summary>
+        /// 拳霸Q1摇杆
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void QuanbaQ1_Click(object sender, RoutedEventArgs e)
+        {
+            QuanbaQ1Controller window = new QuanbaQ1Controller();
+            window.Show();
+        }
+
+        /// <summary>
+        /// XboxOne手柄
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void XboxOne_Click(object sender, RoutedEventArgs e)
+        {
+            XboxOneController window = new XboxOneController();
+            window.Show();
         }
     }
 }
